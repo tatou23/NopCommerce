d749ec91dcd20fefcd5ce95f9b51f3b600714150
COULIBALY ASSETOU <astou@MacBookPro.lan>
2025-07-07 19:58:27 +0200

    Log in and log out tests cases added

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,30 @@
+describe("Log in a user",function(){
+    it("Should log in successfully a user",function(){
+        cy.visit('http://automationexercise.com');
+        cy.contains('AutomationExercise');
+        cy.contains('a', "Signup / Login").click();
+        cy.contains("Login to your account");
+        cy.fixture("user").then((userData)=>{
+            cy.get("[data-qa='login-email']").type(userData.signupEmail);
+        })
+        cy.fixture("account_info").then((accountInfo)=>{
+            cy.get("[data-qa='login-password']").type(accountInfo.password);
+            cy.get("[data-qa='login-button']").click();
+            cy.contains('a', " Logged in as ");
+        })
+    })
+    it("Should fail to log in a user with bad credentials",function(){
+        cy.visit('http://automationexercise.com');
+        cy.contains('AutomationExercise');
+        cy.contains('a', "Signup / Login").click();
+        cy.contains("Login to your account");
+        cy.fixture("user").then((userData)=>{
+            cy.get("[data-qa='login-email']").type(userData.badEmail);
+        })
+        cy.fixture("account_info").then((accountInfo)=>{
+            cy.get("[data-qa='login-password']").type(accountInfo.badPassword);
+            cy.get("[data-qa='login-button']").click();
+            cy.contains('p', "Your email or password is incorrect!");
+        })
+    })
+})
---
+++ cypress/e2e/logout.cy.js
@@ -0,0 +1,18 @@
+describe("Log out a user",function(){
+    it("Should log out a user", function(){
+        cy.visit('http://automationexercise.com');
+        cy.contains('AutomationExercise');
+        cy.contains('a', "Signup / Login").click();
+        cy.contains("Login to your account");
+        cy.fixture("user").then((userData)=>{
+            cy.get("[data-qa='login-email']").type(userData.signupEmail);
+        })
+        cy.fixture("account_info").then((accountInfo)=>{
+            cy.get("[data-qa='login-password']").type(accountInfo.password);
+            cy.get("[data-qa='login-button']").click();
+            cy.contains('a', " Logged in as ");
+        })
+        cy.contains('a', "Logout").click();
+        cy.contains('a', "Signup / Login")
+    })
+})
