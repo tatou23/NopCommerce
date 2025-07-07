b75aad3da9a2fad9b737aafaf333a6ce9f8a2473
COULIBALY ASSETOU <astou@MacBookPro.lan>
2025-07-07 22:05:48 +0200

    Fix on registration

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,10 +1,6 @@
 describe('Register a new user',function(){
-    // beforeEach(function(){
+    /*Redirection on the registration page*/
 
-    //     cy.visit('http://automationexercise.com');
-    //     cy.contains('AutomationExercise');
-    //     cy.contains('a', "Signup / Login").click();
-    // })
     it('Should redirect on the registration page', function(){
         cy.visit('http://automationexercise.com');
         cy.contains('AutomationExercise');
@@ -12,6 +8,8 @@ describe('Register a new user',function(){
         cy.url().should('include', '/login');
         cy.contains('New User Signup!');
     })
+    /*Registration of a new user*/
+
     it('Should register a new user', function(){
         cy.visit('http://automationexercise.com');
         cy.contains('AutomationExercise');
@@ -43,30 +41,6 @@ describe('Register a new user',function(){
         })
         
     })
-    // it('Should fill the user account informations', function(){
-    //     cy.visit('https://automationexercise.com/signup');
-    //     cy.contains('Enter Account Information');
-    //     cy.fixture('account_info').then((accountInfo) => {
-    //         cy.get('#uniform-id_gender1').click();
-    //         cy.get('[data-qa="password"]').type(accountInfo.password);
-    //         cy.get('[data-qa="days"]').type(accountInfo.day);
-    //         cy.get('[data-qa="month"]').type(accountInfo.month);
-    //         cy.get('[data-qa="years"]').type(accountInfo.year);
-    //         cy.get('[data-qa="first_name"]').type(accountInfo.firstName);
-    //         cy.get('[data-qa="last_name"]').type(accountInfo.lastName);
-    //         cy.get('[data-qa="address"]').type(accountInfo.address);
-    //         cy.get('[data-qa="country"]').type(accountInfo.country);
-    //         cy.get('[data-qa="state"]').type(accountInfo.state);
-    //         cy.get('[data-qa="city"]').type(accountInfo.city);
-    //         cy.get('[data-qa="zipcode"]').type(accountInfo.zipCode);
-    //         cy.get('[data-qa="mobile_number"]').type(accountInfo.mobileNumber);
-    //         cy.get('[data-qa="create-account"]').click()
-
-
-    //     })
-        
-    // })
-
     
 
 })
